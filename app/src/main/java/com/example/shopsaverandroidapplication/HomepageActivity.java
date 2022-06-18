c0982185d64652c6457295033be95b89d78e774b
kangzongxian <kangzongxian88@gmail.com>
2022-06-18 16:48:05 +0800

    Added Auto Login and Signout

--- app/src/main/java/com/example/shopsaverandroidapplication/HomepageActivity.java
+++ app/src/main/java/com/example/shopsaverandroidapplication/HomepageActivity.java
@@ -1,10 +1,16 @@
 package com.example.shopsaverandroidapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 // This Activity is responsible for the main page of the class
 // When clicked on ItemListActivity, user will be taken to see a list of items being tracked
@@ -12,6 +18,11 @@ import android.widget.Button;
 
 // TODO: Will do up the homepage stuff
 public class HomepageActivity extends AppCompatActivity {
+
+    // Firebase Stuff
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
 
     // Get my widgets
     private Button trackingListButton;
@@ -21,6 +32,10 @@ public class HomepageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        // Assign Firebase Stuff
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
 
         // Assign my widgets
         trackingListButton = findViewById(R.id.button_tracking_list);
@@ -38,7 +53,33 @@ public class HomepageActivity extends AppCompatActivity {
         searchButton.setOnClickListener(view -> {
             startActivity(new Intent(HomepageActivity.this, SearchActivity.class));
         });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu (with the signout option)
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        // Engage actions depending on what option is selected
+        switch (item.getItemId()) {
+            case R.id.action_signout:
+                // Sign user out if current user exists
+                if (user != null && firebaseAuth != null) {
+                    // Use the inbuilt signOut() function
+                    firebaseAuth.signOut();
+
+                    // Move back to the GetStarted Page
+                    // That is the MainActivity
+                    startActivity(new Intent(HomepageActivity.this,
+                            MainActivity.class));
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/shopsaverandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/shopsaverandroidapplication/MainActivity.java
@@ -1,17 +1,43 @@
 package com.example.shopsaverandroidapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import util.ShopSaverApi;
+
 // This class handles the initial page that the user will see
+// It is like a Get Started Page
+// If there already exists a user session ongoing, we move directly to the Homepage
 // When Get Started button is clicked, they will be moved to the Login Page
 public class MainActivity extends AppCompatActivity {
 
     // Initialise my widgets
     private Button getStartedButton;
+
+    // Initialise Firebase Stuff
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    // Establish connection to Database
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    // Get the path to the "Users" part of DB
+    private CollectionReference collectionReference = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +47,51 @@ public class MainActivity extends AppCompatActivity {
         // Assign my widgets
         getStartedButton = findViewById(R.id.get_started_button);
 
+        // If we already have a user, we should just get the user details straight away
+        // Then we take user to the Homepage
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+                // If a user exists, get the UserId, and find user details
+                if (currentUser != null) {
+                    currentUser = firebaseAuth.getCurrentUser();
+                    String currentUserId = currentUser.getUid();
+
+                    // Find the user details
+                    collectionReference.whereEqualTo("userId", currentUserId)
+                            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                                    // If havde error, do not proceed
+                                    if (error != null) {
+                                        return;
+                                    }
+
+                                    // Set the ShopSaverApi to have details of that user
+                                    // And we move to the Homepage Activity
+                                    if (!value.isEmpty()) {
+                                        for (QueryDocumentSnapshot snapshot : value) {
+                                            ShopSaverApi shopSaverApi = ShopSaverApi.getInstance();
+                                            shopSaverApi.setUserId(snapshot.getString("userId"));
+                                            shopSaverApi.setUsername(snapshot.getString("username"));
+                                            shopSaverApi.setUserEmail(snapshot.getString("userEmail"));
+
+                                            startActivity(new Intent(MainActivity.this,
+                                                    HomepageActivity.class));
+
+                                            // Finish since we do not want to come back
+                                            // To this MainActivity
+                                            finish();
+                                        }
+                                    }
+                                }
+                            });
+                }
+            }
+        };
+
         // Assign a Click Listener to getStartedButton
         // When getStartedButton is clicked, user will be brought to the LoginActivity
         // They will not be brought back to this activity
@@ -28,5 +99,14 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
             finish();
         });
+    }
+
+    // When we start, engage the Firebase variables
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
     }
 }
